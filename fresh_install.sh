97587752678d8bea00b5e48ee56d8189ed172ab3
MsotoM <xanathos.cl@gmail.com>
2017-03-20 14:47:14 -0300

    Add files via upload

---
+++ fresh_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt-get update
+echo "Done apt-get"
+echo "Installing PHP5-CLI CURL"
+apt-get -y install php5-cli curl
+echo "Done"
+cd /tmp
+wget https://github.com/MsotoM/FOSPunga/blob/master/install_panel.php
+php install.php
+wget https://github.com/MsotoM/FOSPunga/blob/master/db_install.sh
+chmod 755 db_install.sh
+./db_install.sh
